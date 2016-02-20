fb79c6e8ceac2b8cf8a7c955e98176d9955b3bce
wyu <kharma@gmail.com>
2016-02-20 15:53:09 -0800

    w4: Update .gitignore

---
+++ bash/do-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# digital ocean droplet, dwa-lamp-14.04 IP: 107.170.222.81
+
+# ssh root@107.170.222.81
+# apt-get update
+# sudo apt-get install git
+
+# cd /usr/local/bin
+# curl -sS https://getcomposer.org/installer | sudo php
+# sudo mv composer.phar composer
+
+# sudo a2enmod rewrite
+# sudo service apache2 restart
+# composer install --prefer-dist
