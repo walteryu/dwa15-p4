53bbb51477373889642973f0bbddec1ff335e7da
wyu <kharma@gmail.com>
2016-02-20 15:54:36 -0800

    w4: Update bash script

--- bash/update-dev.sh
+++ bash/update-dev.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# digital ocean droplet, dwa-lamp-14.04 IP: 107.170.222.81
-
-# ssh root@107.170.222.81
-# apt-get update
-# sudo apt-get install git
-
-# cd /usr/local/bin
-# curl -sS https://getcomposer.org/installer | sudo php
-# sudo mv composer.phar composer
-
-# sudo a2enmod rewrite
-# sudo service apache2 restart
-# composer install --prefer-dist
+apt-get update
+sudo service apache2 restart
+composer install --prefer-dist
