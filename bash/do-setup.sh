cb24abb0e5a2f7b69151b3befee572cad0d51d23
wyu <kharma@gmail.com>
2016-02-21 20:18:20 -0800

    w4: update bash script

--- bash/do-setup.sh
+++ bash/do-setup.sh
@@ -12,3 +12,34 @@
 # sudo a2enmod rewrite
 # sudo service apache2 restart
 # composer install --prefer-dist
+
+# set permissions inside from within app folder
+# sudo chown -R www-data storage
+
+# update .env file and generate application key
+
+# APP_ENV=production
+# APP_DEBUG=false
+# APP_KEY=longrandomstring
+
+# DB_HOST=localhost
+# DB_DATABASE=homestead
+# DB_USERNAME=homestead
+# DB_PASSWORD=secret
+
+# CACHE_DRIVER=file
+# SESSION_DRIVER=file
+# QUEUE_DRIVER=sync
+
+# MAIL_DRIVER=smtp
+# MAIL_HOST=mailtrap.io
+# MAIL_PORT=2525
+# MAIL_USERNAME=null
+# MAIL_PASSWORD=null
+# MAIL_ENCRYPTION=null
+
+# php artisan key:generate
+
+# update apache localhost file
+# cd /etc/apache2/sites-enabled/
+# vi 000-default.conf
