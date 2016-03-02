7627638e0694f2983b836a442fcb83aa01cc3634
wyu <kharma@gmail.com>
2016-03-01 21:39:28 -0800

    w5: add example routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,24 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['middleware' => ['web']], function () {
-    //
+
+  Route::get('/', function () {
+    # return view('welcome');
+    return 'Hello, welcome to my new Laravel application!';
+  });
+
+  Route::get('/book/{title}', function ($title) {
+    return 'Show an individual book: '.$title;
+  });
+
+  Route::get('/books', function () {
+    return 'Show a list of all books';
+  });
+
+  Route::get('/book/create', function () {
+    $view = '<form>';
+    $view = '<input type="text" name="title">';
+    $view = '</form>';
+  });
+
 });
