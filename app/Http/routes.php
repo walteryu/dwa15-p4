a94a93425018a5cb52c14cf432ea0926291fed0c
wyu <kharma@gmail.com>
2016-03-04 10:21:02 -0800

    w5: update route/controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -35,7 +35,8 @@ Route::group(['middleware' => ['web']], function () {
   });
 
   Route::get('/practice', function() {
-    echo 'Hello World!';
+    echo 'Your current environment: ';
+    echo App::environment();
   });
 
 });
