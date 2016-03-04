df789f018a5d7ffdef75094e1b033e85a2359eb6
wyu <kharma@gmail.com>
2016-03-04 10:35:03 -0800

    w5: resources/views/layouts/

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+    <title>
+        {{-- Yield the title if it exists, otherwise default to 'Foobooks' --}}
+        @yield('title','Foobooks')
+    </title>
+
+    <meta charset='utf-8'>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
+    @yield('head')
+
+</head>
+<body>
+
+    <header>
+        <img
+        src='http://making-the-internet.s3.amazonaws.com/laravel-foobooks-logo@2x.png'
+        style='width:300px'
+        alt='Foobooks Logo'>
+    </header>
+
+    <section>
+        {{-- Main page content will be yielded here --}}
+        @yield('content')
+    </section>
+
+    <footer>
+        &copy; {{ date('Y') }}
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
+    @yield('body')
+
+</body>
+</html>
