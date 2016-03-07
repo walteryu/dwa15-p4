f67e6738be15086ee3bd7d3c30b8999599c99e87
wyu <kharma@gmail.com>
2016-03-07 00:27:02 -0800

    p3: update routes

--- app/Http/Controllers/RandomUserController.php
+++ app/Http/Controllers/RandomUserController.php
@@ -14,9 +14,9 @@ class RandomUserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function getIndex()
     {
-        //
+        return view('random-user.index');
     }
 
     /**
