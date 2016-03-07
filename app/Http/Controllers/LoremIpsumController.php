2c499545e7c802c4a971316f6e258172251e46f5
wyu <kharma@gmail.com>
2016-03-07 00:26:56 -0800

    p3: update routes

--- app/Http/Controllers/LoremIpsumController.php
+++ app/Http/Controllers/LoremIpsumController.php
@@ -14,9 +14,9 @@ class LoremIpsumController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function getIndex()
     {
-        //
+        return view('lorem-ipsum.index');
     }
 
     /**
