a0351005e38a9e1b977e8f10ce8233a342583c4e
wyu <kharma@gmail.com>
2016-03-08 11:09:46 -0800

    p3: organize views

--- resources/views/random-user/index.blade.php
+++ resources/views/random-user/index.blade.php
@@ -15,7 +15,32 @@ such as a page specific stylesheets.
 @stop
 
 @section('content')
-    <h1>Insert web form here...</h1>
+    <h1>Random User Generator</h1>
+
+    <div class="form-group">
+      <p>
+        <label for="count">Number of Words? </label>
+        <input type="number" id="count" name="count" min="1" max="9">
+        <label for="count"> (Between 1-9)</label>
+      </p>
+      <p>
+        <input type="checkbox" id="numbers" name="numbers" value="add_number">
+        <label for="numbers">Add a Number to Last Passphrase?</label>
+      </p>
+      <p>
+        <input type="checkbox" id="characters" name="characters" value="add_char">
+        <label for="characters">Add a Character to Last Passphrase?</label>
+      </p>
+      <p>
+        <input type="checkbox" id="shuffle" name="shuffle" value="shuffle_words">
+        <label for="shuffle">Shuffle Passphrases Again After Selection?</label>
+      </p>
+      <p>
+        <button type="submit" class="btn btn-primary">Generate Password</button>
+      </p>
+    </div>
+  </form>
+
 @stop
 
 {{--
