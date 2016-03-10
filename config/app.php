dfd489e670bee6357cb6ef4d7c1822fa410797f3
wyu <kharma@gmail.com>
2016-03-09 20:55:38 -0800

    p3: add badcow\lorem-ipsum package alias

--- config/app.php
+++ config/app.php
@@ -202,6 +202,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        'LoremIpsumGenerator' => 'Badcow\LoremIpsum\Generator',
+
     ],
 
 ];
