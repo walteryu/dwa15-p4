252ead00d8ede035f0ef30fb1ed114d133b5293d
wyu <kharma@gmail.com>
2016-03-11 09:28:24 -0800

    p3: push logic back to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,11 +23,23 @@ Route::group(['middleware' => ['web']], function () {
   Route::post('/book/create', 'BookController@postCreate');
   Route::get('/book/show/{title?}', 'BookController@getShow');
 
-  Route::get('/books/{category}', function($category) {
-    return 'Books with category: '.$category;
+  # Returns paragraphs based on number parameter
+  Route::get('/lorem-output/{number}', function($number) {
+    $generator = new LoremIpsumGenerator();
+    $paragraphs = $generator->getParagraphs($number);
+    echo implode('<p>', $paragraphs);
+
+    return view('lorem-ipsum/index');
+  });
+
+  # Return random users
+  Route::get('/faker-output', function() {
+    $faker = Faker\Factory::create();
+    echo $faker->name.'<br>';
+    echo $faker->email.'<br>';
+    echo $faker->address.'<br>';
   });
 
   Route::get('/lorem-ipsum', 'LoremIpsumController@getIndex');
   Route::get('/random-user', 'RandomUserController@getIndex');
-
 });
