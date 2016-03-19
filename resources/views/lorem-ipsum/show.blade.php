6118fe482536a72116c491d39f8f4e2ef1f9eded
wyu <kharma@gmail.com>
2016-03-18 17:31:51 -0700

    p3: add getShow view

---
+++ resources/views/lorem-ipsum/show.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Show Book</title>
+    <meta charset='utf-8'>
+    <link href="/css/lorem-ipsum.css" type='text/css' rel='stylesheet'>
+</head>
+<body>
+
+    <header>
+        <h1>Lorem Ipsum Generator Output...</h1>
+    </header>
+
+    <section>
+        <h1>Number of Paragraphs: {{ $title }}</h1>
+    </section>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+</body>
+</html>
