8050ff15ba158e8ad5690e48939b724f758d3960
wyu <kharma@gmail.com>
2016-03-18 21:16:21 -0700

    p3: setup index page

--- resources/views/lorem-ipsum/index.blade.php
+++ resources/views/lorem-ipsum/index.blade.php
@@ -16,52 +16,7 @@ such as a page specific stylesheets.
 
 @section('content')
     <h3>Lorem Ipsum Generator</h3>
-
-    <form method='POST' action='/lorem-ipsum/create'>
-
-        {{ csrf_field() }}
-
-        <div class='form-group'>
-           <label>* Paragraph Count:</label>
-           <input
-               type='number'
-               id='count'
-               name='count'
-               value='{{ old('count') }}'
-           >
-           <div class='error'>{{ $errors->first('count') }}</div>
-        </div>
-
-        <!--
-          <div class='form-group'>
-             <label>* Author:</label>
-             <input
-                 type='text'
-                 id='author'
-                 name='author'
-                 value='{{ old('author') }}'
-             >
-             <div class='error'>{{ $errors->first('author') }}</div>
-          </div>
-        -->
-
-        <button type="submit" class="btn btn-primary">Add Some Lorem!</button>
-
-        {{--
-        <ul class=''>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-        --}}
-
-        <div class='error'>
-            @if(count($errors) > 0)
-                Please correct the errors above and try again.
-            @endif
-        </div>
-
-    </form>
+    <h3>Links Here...</h3>
 @stop
 
 {{--
