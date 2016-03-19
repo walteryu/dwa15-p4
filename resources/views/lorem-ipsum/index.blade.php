eccbf5d4534f63568a5e4f2f1647e129a8ffd39e
wyu <kharma@gmail.com>
2016-03-18 17:31:38 -0700

    p3: setting up views for route/controller

--- resources/views/lorem-ipsum/index.blade.php
+++ resources/views/lorem-ipsum/index.blade.php
@@ -19,22 +19,26 @@ such as a page specific stylesheets.
 
     <div class="form-group">
       <p>
-        <label for="count">Number of Words? </label>
+        <label for="count">Number of Paragraphs? </label>
         <input type="number" id="count" name="count" min="1" max="9">
         <label for="count"> (Between 1-9)</label>
       </p>
-      <p>
-        <input type="checkbox" id="numbers" name="numbers" value="add_number">
-        <label for="numbers">Add a Number to Last Passphrase?</label>
-      </p>
-      <p>
-        <input type="checkbox" id="characters" name="characters" value="add_char">
-        <label for="characters">Add a Character to Last Passphrase?</label>
-      </p>
-      <p>
-        <input type="checkbox" id="shuffle" name="shuffle" value="shuffle_words">
-        <label for="shuffle">Shuffle Passphrases Again After Selection?</label>
-      </p>
+
+      <!--
+        <p>
+          <input type="checkbox" id="numbers" name="numbers" value="add_number">
+          <label for="numbers">Add a Number to Last Passphrase?</label>
+        </p>
+        <p>
+          <input type="checkbox" id="characters" name="characters" value="add_char">
+          <label for="characters">Add a Character to Last Passphrase?</label>
+        </p>
+        <p>
+          <input type="checkbox" id="shuffle" name="shuffle" value="shuffle_words">
+          <label for="shuffle">Shuffle Passphrases Again After Selection?</label>
+        </p>
+      -->
+
       <p>
         <button type="submit" class="btn btn-primary">Generate Random Text</button>
       </p>
