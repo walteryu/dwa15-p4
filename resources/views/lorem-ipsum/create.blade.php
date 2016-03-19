e90c5012f72be33746d1b03aa1ebfe07a54d2c34
wyu <kharma@gmail.com>
2016-03-18 19:41:18 -0700

    p3: add/test create view

---
+++ resources/views/lorem-ipsum/create.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.master')
+
+@section('title')
+    Create Some Lorem
+@stop
+
+@section('content')
+
+    <h3>Create Some Lorem</h3>
+
+    <form method='POST' action='/lorem-ipsum/create'>
+
+        {{ csrf_field() }}
+
+        <div class='form-group'>
+           <label>* Paragraph Count:</label>
+           <input
+               type='number'
+               id='count'
+               name='count'
+               value='{{ old('count') }}'
+           >
+           <div class='error'>{{ $errors->first('count') }}</div>
+        </div>
+
+        <!--
+          <div class='form-group'>
+             <label>* Author:</label>
+             <input
+                 type='text'
+                 id='author'
+                 name='author'
+                 value='{{ old('author') }}'
+             >
+             <div class='error'>{{ $errors->first('author') }}</div>
+          </div>
+        -->
+
+        <button type="submit" class="btn btn-primary">Add Some Lorem!</button>
+
+        {{--
+        <ul class=''>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+        --}}
+
+        <div class='error'>
+            @if(count($errors) > 0)
+                Please correct the errors above and try again.
+            @endif
+        </div>
+
+    </form>
+
+@stop
