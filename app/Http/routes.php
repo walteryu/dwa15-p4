37791463f8372eaae447284547da291f3a1150be
wyu <kharma@gmail.com>
2016-03-19 13:56:18 -0700

    p3: clean up routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,19 +1,11 @@
 <?php
 
-# Rooted to public/dir in htdocs/MAMP
 Route::group(['middleware' => ['web']], function () {
 
   Route::get('/', function () {
-    // return view('welcome');
     return view('lorem-ipsum/index');
   });
 
-  Route::get('/env', function() {
-    echo 'Your current environment: ';
-    echo App::environment();
-  });
-
-  # Returns paragraphs based on number parameter
   Route::get('/lorem-output/{number}', function($number) {
     $generator = new LoremIpsumGenerator();
     $paragraphs = $generator->getParagraphs($number);
