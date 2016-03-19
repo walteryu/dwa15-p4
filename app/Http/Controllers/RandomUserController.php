79b4eb1eef59f5befe76bf1eba59d49b356a60cc
wyu <kharma@gmail.com>
2016-03-19 10:14:53 -0700

    p3: clean up controller code

--- app/Http/Controllers/RandomUserController.php
+++ app/Http/Controllers/RandomUserController.php
@@ -32,35 +32,22 @@ class RandomUserController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for creating a new resource.
      *
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function create()
     {
         //
     }
 
     /**
-     * Update the specified resource in storage.
+     * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function store(Request $request)
     {
         //
     }
