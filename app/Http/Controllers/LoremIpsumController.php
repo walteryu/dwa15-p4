eb6d378eceb7d02baf5fd29b43458085326325c6
wyu <kharma@gmail.com>
2016-03-18 19:39:01 -0700

    p3: remove show method

--- app/Http/Controllers/LoremIpsumController.php
+++ app/Http/Controllers/LoremIpsumController.php
@@ -69,17 +69,6 @@ class LoremIpsumController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
