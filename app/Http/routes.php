7b3e6e85e1f136e029569269d74fc8d366fc86c6
wyu <kharma@gmail.com>
2016-03-20 14:33:05 -0700

    p3: increase to 4-line indentation

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,32 +2,32 @@
 
 Route::group(['middleware' => ['web']], function () {
 
-  Route::get('/', function () {
-    return view('lorem-ipsum/index');
-  });
+    Route::get('/', function () {
+        return view('lorem-ipsum/index');
+    });
 
-  Route::get('/lorem-output/{number}', function($number) {
-    $generator = new LoremIpsumGenerator();
-    $paragraphs = $generator->getParagraphs($number);
-    echo implode('<p>', $paragraphs);
+    Route::get('/lorem-output/{number}', function($number) {
+        $generator = new LoremIpsumGenerator();
+        $paragraphs = $generator->getParagraphs($number);
+        echo implode('<p>', $paragraphs);
 
-    return view('lorem-ipsum/index');
-  });
+        return view('lorem-ipsum/index');
+    });
 
-  Route::get('/faker-output', function() {
-    $user = Faker\Factory::create();
-    echo $user->name.'<br>';
-    echo $user->email.'<br>';
-    echo $user->address.'<br>';
-  });
+    Route::get('/faker-output', function() {
+        $user = Faker\Factory::create();
+        echo $user->name.'<br>';
+        echo $user->email.'<br>';
+        echo $user->address.'<br>';
+    });
 
-  Route::get('/lorem-ipsum', 'LoremIpsumController@getIndex');
-  Route::get('/lorem-ipsum/create', 'LoremIpsumController@getCreate');
-  Route::post('/lorem-ipsum/create', 'LoremIpsumController@postCreate');
-  Route::get('/lorem-ipsum/show/{count?}', 'LoremIpsumController@getShow');
+    Route::get('/lorem-ipsum', 'LoremIpsumController@getIndex');
+    Route::get('/lorem-ipsum/create', 'LoremIpsumController@getCreate');
+    Route::post('/lorem-ipsum/create', 'LoremIpsumController@postCreate');
+    Route::get('/lorem-ipsum/show/{count?}', 'LoremIpsumController@getShow');
 
-  Route::get('/random-user', 'RandomUserController@getIndex');
-  Route::get('/random-user/create', 'RandomUserController@getCreate');
-  Route::post('/random-user/create', 'RandomUserController@postCreate');
-  Route::get('/random-user/show/{count?}', 'RandomUserController@getShow');
+    Route::get('/random-user', 'RandomUserController@getIndex');
+    Route::get('/random-user/create', 'RandomUserController@getCreate');
+    Route::post('/random-user/create', 'RandomUserController@postCreate');
+    Route::get('/random-user/show/{count?}', 'RandomUserController@getShow');
 });
