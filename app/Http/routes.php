84c51364f50e6bc762a05b78520788c9dca120ac
wyu <kharma@gmail.com>
2016-03-22 21:00:17 -0700

    p3: remove test output routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,26 +1,7 @@
 <?php
 
 Route::group(['middleware' => ['web']], function () {
-
-    Route::get('/', function () {
-        return view('lorem-ipsum/index');
-    });
-
-    Route::get('/lorem-output/{number}', function($number) {
-        $generator = new LoremIpsumGenerator();
-        $paragraphs = $generator->getParagraphs($number);
-        echo implode('<p>', $paragraphs);
-
-        return view('lorem-ipsum/index');
-    });
-
-    Route::get('/faker-output', function() {
-        $user = Faker\Factory::create();
-        echo $user->name.'<br>';
-        echo $user->email.'<br>';
-        echo $user->address.'<br>';
-    });
-
+    Route::get('/', 'LoremIpsumController@getIndex');
     Route::get('/lorem-ipsum', 'LoremIpsumController@getIndex');
     Route::get('/lorem-ipsum/create', 'LoremIpsumController@getCreate');
     Route::post('/lorem-ipsum/create', 'LoremIpsumController@postCreate');
