56a04a9b991350764f4f76d4bd50c6d472331bc5
wyu <kharma@gmail.com>
2016-03-22 21:01:15 -0700

    p3: remove foobooks boilerplate comments

--- resources/views/random-user/index.blade.php
+++ resources/views/random-user/index.blade.php
@@ -37,12 +37,6 @@
 
 @stop
 
-{{--
-This `body` section will be yielded right before the closing </body> tag.
-Use it to add specific things that *this* View needs at the end of the body,
-such as a page specific JavaScript files.
---}}
-
 @section('body')
     <script src="/js/random-user/index.js"></script>
 @stop
