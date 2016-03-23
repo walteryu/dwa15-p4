92bb477613852101dc85bc18e906d7c65402b66a
wyu <kharma@gmail.com>
2016-03-22 21:01:26 -0700

    p3: remove foobooks boilerplate comments

--- resources/views/random-user/show.blade.php
+++ resources/views/random-user/show.blade.php
@@ -24,12 +24,6 @@
     @endforelse
 @stop
 
-{{--
-This `body` section will be yielded right before the closing </body> tag.
-Use it to add specific things that *this* View needs at the end of the body,
-such as a page specific JavaScript files.
---}}
-
 @section('body')
     <script src="/js/random-user.js"></script>
 @stop
