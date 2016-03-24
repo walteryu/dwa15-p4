7fbd45166aada7c2e1ff381f4a219a6a56e017d8
wyu <kharma@gmail.com>
2016-03-23 23:39:07 -0700

    p3: debugging jquery validation file

--- public/js/random-user.js
+++ public/js/random-user.js
@@ -18,6 +18,23 @@ jQuery(document).ready(function () {
                 required: true
             }
         }
+        messages: {
+            count: {
+                required: "Please enter number count",
+                range: "Please enter number from 0-9"
+            }
+            add_email: {
+                required: "Please enter value",
+            }
+            add_address: {
+                required: "Please enter value",
+            }
+        }
+
+        submitHandler: function(form) {
+            form.submit();
+        }
+
         highlight: function (element) {
             $(element).closest('.form-group').removeClass('success').addClass('error');
         },
