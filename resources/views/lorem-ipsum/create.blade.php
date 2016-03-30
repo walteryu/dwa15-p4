a074afa56585eb46b8bfe01d0701ae3758535477
wyu <kharma@gmail.com>
2016-03-30 12:02:52 -0700

    p3: add bootstrap error warning

--- resources/views/lorem-ipsum/create.blade.php
+++ resources/views/lorem-ipsum/create.blade.php
@@ -15,12 +15,26 @@
         Create random text below by entering the number between 0-9 and options
     </blockquote>
 
-
     <form method='POST' action='/lorem-ipsum/create'>
 
         {{ csrf_field() }}
 
         <div class='form-group'>
+            <div class="container">
+                <div class="error">
+                    @if(count($errors) > 0)
+                        <!-- Reference: https://laravel.com/docs/6.2/validation -->
+                        <div class="col-md-4 alert alert-danger">
+                            Please correct the errors below and try again:
+                            <p>
+                            @foreach ( $errors->all() as $error )
+                                {{ $error }}
+                            @endforeach
+                        </div>
+                    @endif
+                </div>
+            </div>
+
            <label>* Paragraph Count (Between 0-9):</label>
            <input
                type='number'
