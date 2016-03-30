91d037938878d151c5c4bc79a0e3803f2a86f740
wyu <kharma@gmail.com>
2016-03-30 13:07:03 -0700

    p3: remove show page routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,11 @@
 <?php
-
 Route::group(['middleware' => ['web']], function () {
     Route::get('/', 'LoremIpsumController@getIndex');
     Route::get('/lorem-ipsum', 'LoremIpsumController@getIndex');
     Route::get('/lorem-ipsum/create', 'LoremIpsumController@getCreate');
     Route::post('/lorem-ipsum/create', 'LoremIpsumController@postCreate');
-    Route::get('/lorem-ipsum/show/{count?}', 'LoremIpsumController@getShow');
 
     Route::get('/random-user', 'RandomUserController@getIndex');
     Route::get('/random-user/create', 'RandomUserController@getCreate');
     Route::post('/random-user/create', 'RandomUserController@postCreate');
-    Route::get('/random-user/show/{count?}', 'RandomUserController@getShow');
 });
