94666bc935020447d1cbb3253333a235cf39a8d4
wyu <kharma@gmail.com>
2016-04-16 09:31:31 -0700

    p4: update project model test

---
+++ tests/models/ProjectTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Zizaco\FactoryMuff\Facade\FactoryMuff;
+
+class PostTest extends TestCase
+{
+    public function test_posted_at()
+    {
+        // Instantiate, fill with values, save and return
+        $project = FactoryMuff::create('Project');
+
+        // Regular expression that represents d/m/Y pattern
+        $expected = '/\d{2}\/\d{2}\/\d{4}/';
+
+        // True if preg_match finds the pattern
+        $matches = ( preg_match($expected, $post->postedAt()) ) ? true : false;
+        $this->assertTrue( $matches );
+    }
+}
