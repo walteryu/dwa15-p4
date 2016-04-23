5518b65366bfb6f660cd95eeec7baed8e6cbde4e
wyu <kharma@gmail.com>
2016-04-23 10:35:46 -0700

    p4: correct seeder filename typos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
-        $this->call(ProjectTableSeeder::class);
-        $this->call(InspectionTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(ProjectsTableSeeder::class);
+        $this->call(InspectionsTableSeeder::class);
     }
 }
