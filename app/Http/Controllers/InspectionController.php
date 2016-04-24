098774ce427dcace5a7df45b9573649cf4032bd3
wyu <kharma@gmail.com>
2016-04-24 01:01:35 -0700

    p4: add controller methods

--- app/Http/Controllers/InspectionController.php
+++ app/Http/Controllers/InspectionController.php
@@ -8,5 +8,72 @@ use P3\Http\Requests;
 
 class InspectionController extends Controller
 {
-    //
+    function getIndex() {
+        $inspections = \App\Inspection::all();
+        return view('inspections.index')->with('inspections', $inspections);
+    }
+
+    public function getShow($name = null) {
+        return view('inspections.show')->with('name',$name);
+    }
+
+    public function getCreate() {
+        return view('inspections.create');
+    }
+
+    public function postCreate(Request $request) {
+        $this->validate($request,[
+            # 'title' => 'required|min:3',
+            # 'author' => 'required',
+            # 'published' => 'required|min:4',
+            # 'cover' => 'required|url',
+            # 'purchase_link' => 'required|url',
+        ]);
+
+        $data = $request->only(
+            # 'title','author','published','cover','purchase_link'
+        );
+        \App\Inspection::create($data);
+
+        \Session::flash('message','Your inspection was added');
+        return redirect('/inspections');
+    }
+
+    public function getEdit($id) {
+        $inspection = \App\Inspection::find($request->id);
+        return view('inspections.edit')->with('name',$name);
+    }
+
+    public function postEdit(Request $request) {
+        $inspection = \App\Inspection::find($request->id);
+
+        # $book->title = $request->title;
+        # $book->author_id = $request->author_id;
+        # $book->cover = $request->cover;
+        # $book->published = $request->published;
+        # $book->purchase_link = $request->purchase_link;
+
+        $inspection->save();
+
+        \Session::flash('message','Your changes were saved.');
+        return redirect('/inspection/edit/'.$request->id);
+    }
+
+    public function getConfirmDelete($id) {
+        $inspection = \App\Inspection::find($id);
+        return view('inspections.delete')->with('inspection', $inspection);
+    }
+
+    public function getGoDelete($id) {
+        $inspection = \App\Inspection::find($id);
+
+        if(is_null($inspection)) {
+            \Session::flash('flash_message','Inspection not found.');
+            return redirect('/inspections');
+        }
+
+        $inspection->delete();
+        \Session::flash('flash_message','Inspection was deleted.');
+        return redirect('/inspections');
+    }
 }
