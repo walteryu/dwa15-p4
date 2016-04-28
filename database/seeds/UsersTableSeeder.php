2071818cd925df1c9808bd6986e16ec16f4fce17
wyu <kharma@gmail.com>
2016-04-28 10:24:17 -0700

    p4: update user seed file

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,8 @@ class UsersTableSeeder extends Seeder
             'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
             'name' => 'Jamal Harvard',
             'email' => 'jamal@harvard.edu',
-            'password' => 'helloworld',
+            # 'password' => 'helloworld',
+            'password' => \Hash::make('helloworld'),
         ]);
 
         DB::table('users')->insert([
@@ -24,7 +25,22 @@ class UsersTableSeeder extends Seeder
             'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
             'name' => 'Jill Harvard',
             'email' => 'jill@harvard.edu',
-            'password' => 'helloworld',
+            # 'password' => 'helloworld',
+            'password' => \Hash::make('helloworld'),
         ]);
+
+        /*
+          $user = \App\User::firstOrCreate(['email' => 'jill@harvard.edu']);
+          $user->name = 'Jill';
+          $user->email = 'jill@harvard.edu';
+          $user->password = \Hash::make('helloworld');
+          $user->save();
+
+          $user = \App\User::firstOrCreate(['email' => 'jamal@harvard.edu']);
+          $user->name = 'Jamal';
+          $user->email = 'jamal@harvard.edu';
+          $user->password = \Hash::make('helloworld');
+          $user->save();
+        */
     }
 }
