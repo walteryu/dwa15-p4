c48c282a8cac65cd5853ec37082f71f865fa7321
wyu <kharma@gmail.com>
2016-04-28 10:07:27 -0700

    p4: relocate logout route/view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,10 +4,15 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('/login', 'Auth\AuthController@getLogin');
     Route::post('/login', 'Auth\AuthController@postLogin');
 
+    Route::get('/logout', 'Auth\AuthController@logout');
+
+    Route::get('/logout/confirm',function(){
+        session()->flash('message','You have been logged out.');
+        return redirect()->to('/login');
+    });
+
     Route::get('/register', 'Auth\AuthController@getRegister');
     Route::post('/register', 'Auth\AuthController@postRegister');
-
-    Route::get('/logout', 'Auth\AuthController@logout');
 
     Route::get('/show-login-status', function() {
         # You may access the authenticated user via the Auth facade
