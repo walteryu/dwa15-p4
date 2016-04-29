9dd879278837a55eb2f9b5b7da9a4d8d8a456d83
wyu <kharma@gmail.com>
2016-04-29 12:55:21 -0700

    p4: update links

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,7 +44,8 @@
                   <ul class="nav navbar-nav">
                     @if(Auth::check())
                         <li class="active"><a href="/">Home<span class="sr-only">(current)</span></a></li>
-                        <li><a href='/project/create'>Add Project</a></li>
+                        <li><a href='/projects'>Projects</a></li>
+                        <li><a href='/inspections'>Inspections</a></li>
                         <li><a href='/logout'>Log out</a></li>
                     @else
                         <li class="active"><a href="/login">Login<span class="sr-only">(current)</span></a></li>
