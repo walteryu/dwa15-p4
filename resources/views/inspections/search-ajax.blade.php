f66c3489a597097465f7f5d5a900e010b9753c39
wyu <kharma@gmail.com>
2016-05-08 11:03:03 -0700

    p4: update title/heading

--- resources/views/inspections/search-ajax.blade.php
+++ resources/views/inspections/search-ajax.blade.php
@@ -1,9 +1,11 @@
-@if(sizeof($books) == 0)
+@if(sizeof($inspections) == 0)
     No results found.
 @endif
 
-@foreach($books as $book)
-    <div class='book'>
-        <a href='/book/show/{{$book->id}}'>{{ $book->title }}</a>
+@foreach($inspections as $inspection)
+    <div class='inspection'>
+        <h4>
+            <a href='/inspection/show/{{$inspection->id}}'>{{ $inspection->name }} - {{ $inspection->description }}</a>
+        </h4>
     </div>
 @endforeach
