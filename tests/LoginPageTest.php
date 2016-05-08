4df85fb04fd674106f88ab09a36185765d25327f
wyu <kharma@gmail.com>
2016-05-07 21:21:36 -0700

    p4: organize integration tests

---
+++ tests/LoginPageTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LoginPageTest extends TestCase
+{
+    public function testLoginPage()
+    {
+        try
+        {
+            # Test auth/login page
+            $this->visit('/login')
+                ->see('Welcome to StormSafe!')
+                ->dontSee('All Projects')
+                ;
+        }
+        catch(Exception $e)
+        {
+            echo 'Message: '.$e->getMessage();
+        }
+    }
+}
