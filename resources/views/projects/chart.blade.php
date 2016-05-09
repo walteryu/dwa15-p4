9ccbf8dff510b01b99ccba308e5e7d0f65a961f3
wyu <kharma@gmail.com>
2016-05-08 22:26:53 -0700

    p4: organize js chart files

--- resources/views/projects/chart.blade.php
+++ resources/views/projects/chart.blade.php
@@ -9,34 +9,37 @@
 @stop
 
 @section('content')
-    <div class='widget'>
+    <div class='well well-large'>
         <h2>Project Count</h2>
         <h4>Total Projects/Inspections</h4>
-    </div>
-    <br></br>
+        <h4>
+            Chart Generation: <a href="http://d3js.org/">D3.js</a>
+        </h4>
+        <br></br>
 
-    <div id="chart"></div>
-    <script>
-        var chart = c3.generate({
-            data: {
-                columns: [
-                    ['Projects', {{ array_sum($project_count) }} ],
-                    ['Inspections', {{ array_sum($inspection_count) }} ]
-                ],
-                type: 'bar'
-            },
-            bar: {
-                width: {
-                    ratio: 0.5
+        <div id="chart"></div>
+        <script>
+            var chart = c3.generate({
+                data: {
+                    columns: [
+                        ['Projects', {{ array_sum($project_count) }} ],
+                        ['Inspections', {{ array_sum($inspection_count) }} ]
+                    ],
+                    type: 'bar'
+                },
+                bar: {
+                    width: {
+                        ratio: 0.5
+                    }
+                },
+                axis: {
+                    y: {
+                        label: 'Count'
+                    }
                 }
-            },
-            axis: {
-                y: {
-                    label: 'Count'
-                }
-            }
-        });
-    </script>
+            });
+        </script>
+    </div>
 @stop
 
 @section('body')
