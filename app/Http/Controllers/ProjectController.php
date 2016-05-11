fc0a611c9fb914836db861cb0e4112162361725f
wyu <kharma@gmail.com>
2016-05-10 21:22:00 -0700

    p4: add remaining model attributes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -97,7 +97,62 @@ class ProjectController extends Controller
             'address',
             'city',
             'state',
-            'zipcode'
+            'zipcode',
+
+            'latitude',
+            'longitude',
+            'active',
+
+            'tracking_number',
+            'cost_center',
+            'project_phase',
+            'wdid_number',
+            'cgp_number',
+            'risk_level',
+
+            'owner_company_name',
+            'owner_company_description',
+            'owner_company_zipcode',
+            'owner_company_address',
+            'owner_company_city',
+            'owner_company_state',
+            'owner_representative',
+            'owner_title',
+            'owner_phone',
+            'owner_email',
+
+            'contractor_company_name',
+            'contractor_company_description',
+            'contractor_company_zipcode',
+            'contractor_company_address',
+            'contractor_company_city',
+            'contractor_company_state',
+            'contractor_representative',
+            'contractor_title',
+            'contractor_phone',
+            'contractor_email',
+
+            'wpcm_company_name',
+            'wpcm_company_description',
+            'wpcm_company_zipcode',
+            'wpcm_company_address',
+            'wpcm_company_city',
+            'wpcm_company_state',
+            'wpcm_representative',
+            'wpcm_title',
+            'wpcm_phone',
+            'wpcm_email',
+
+            'qsp_company_name',
+            'qsp_company_description',
+            'qsp_company_zipcode',
+            'qsp_company_address',
+            'qsp_company_city',
+            'qsp_company_state',
+            'qsp_representative',
+            'qsp_title',
+            'qsp_phone',
+            'qsp_email'
         );
         $data = array_values($data);
 
