f4c9c1b83c137085a418bcdf04de20971af11968
wyu <kharma@gmail.com>
2016-05-11 00:10:55 -0700

    p4: add remaining model attributes

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -35,19 +35,264 @@
                 </h3>
                 <h3>
                     Street Address:&nbsp;
-                    {{ $value->address}}
+                    {{ $value->address }}
                 </h3>
                 <h3>
                     City:&nbsp;
-                    {{ $value->city}}
+                    {{ $value->city }}
                 </h3>
                 <h3>
                     State:&nbsp;
-                    {{ $value->state}}
+                    {{ $value->state }}
                 </h3>
                 <h3>
                     Zipcode:&nbsp;
-                    {{ $value->zipcode}}
+                    {{ $value->zipcode }}
+                </h3>
+
+                <h3 class='form-group'>
+                    latitude:
+                    {{ $value->latitude }}
+                </h3>
+
+                <h3 class='form-group'>
+                    longitude:
+                    {{ $value->longitude }}
+                </h3>
+
+                <h3 class='form-group'>
+                    active:
+                    {{ $value->active }}
+                </h3>
+
+                <h3 class='form-group'>
+                    tracking_number:
+                    {{ $value->tracking_number }}
+                </h3>
+
+                <h3 class='form-group'>
+                    cost_center:
+                    {{ $value->cost_center }}
+                </h3>
+
+                <h3 class='form-group'>
+                    project_phase:
+                    {{ $value->project_phase }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wdid_number:
+                    {{ $value->wdid_number }}
+                </h3>
+
+                <h3 class='form-group'>
+                    cgp_number:
+                    {{ $value->cgp_number }}
+                </h3>
+
+                <h3 class='form-group'>
+                    risk_level:
+                    {{ $value->risk_level }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_company_name:
+                    {{ $value->owner_company_name }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_company_description:
+                    {{ $value->owner_company_description }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_company_zipcode:
+                    {{ $value->owner_company_zipcode }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_company_address:
+                    {{ $value->owner_company_address }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_company_city:
+                    {{ $value->owner_company_city }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_company_state:
+                    {{ $value->owner_company_state }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_representative:
+                    {{ $value->owner_representative }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_title:
+                    {{ $value->owner_title }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_phone:
+                    {{ $value->owner_phone }}
+                </h3>
+
+                <h3 class='form-group'>
+                    owner_email:
+                    {{ $value->owner_email }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_company_name:
+                    {{ $value->contractor_company_name }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_company_description:
+                    {{ $value->contractor_company_description }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_company_zipcode:
+                    {{ $value->contractor_company_zipcode }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_company_address:
+                    {{ $value->contractor_company_address }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_company_city:
+                    {{ $value->contractor_company_city }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_company_state:
+                    {{ $value->contractor_company_state }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_representative:
+                    {{ $value->contractor_representative }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_title:
+                    {{ $value->contractor_title }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_phone:
+                    {{ $value->contractor_phone }}
+                </h3>
+
+                <h3 class='form-group'>
+                    contractor_email:
+                    {{ $value->contractor_email }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_company_name:
+                    {{ $value->wpcm_company_name }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_company_description:
+                    {{ $value->wpcm_company_description }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_company_zipcode:
+                    {{ $value->wpcm_company_zipcode }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_company_address:
+                    {{ $value->wpcm_company_address }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_company_city:
+                    {{ $value->wpcm_company_city }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_company_state:
+                    {{ $value->wpcm_company_state }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_representative:
+                    {{ $value->wpcm_representative }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_title:
+                    {{ $value->wpcm_title }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_phone:
+                    {{ $value->wpcm_phone }}
+                </h3>
+
+                <h3 class='form-group'>
+                    wpcm_email:
+                    {{ $value->wpcm_email }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_company_name:
+                    {{ $value->qsp_company_name }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_company_description:
+                    {{ $value->qsp_company_description }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_company_zipcode:
+                    {{ $value->qsp_company_zipcode }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_company_address:
+                    {{ $value->qsp_company_address }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_company_city:
+                    {{ $value->qsp_company_city }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_company_state:
+                    {{ $value->qsp_company_state }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_representative:
+                    {{ $value->qsp_representative }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_title:
+                    {{ $value->qsp_title }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_phone:
+                    {{ $value->qsp_phone }}
+                </h3>
+
+                <h3 class='form-group'>
+                    qsp_email:
+                    {{ $value->qsp_email }}
                 </h3>
             @endforeach
 
