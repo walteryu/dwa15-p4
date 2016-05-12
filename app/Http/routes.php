119409ad9fa6cfc36085576f325781034b3c576d
wyu <kharma@gmail.com>
2016-05-12 12:03:30 -0700

    p4: refactor/organize code before deadline

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,74 +14,7 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('/register', 'Auth\AuthController@getRegister');
     Route::post('/register', 'Auth\AuthController@postRegister');
 
-    Route::get('/show-login-status', function() {
-        $user = Auth::user();
-
-        if($user) {
-            echo 'You are logged in.';
-            dump($user->toArray());
-        } else {
-            echo 'You are not logged in.';
-        }
-
-        return;
-    });
-
-    Route::get('/debug', function() {
-        echo '<pre>';
-
-        echo '<h1>Environment</h1>';
-        echo App::environment().'</h1>';
-
-        echo '<h1>Debugging?</h1>';
-        if(config('app.debug')) echo "Yes"; else echo "No";
-
-        echo '<h1>Database Config</h1>';
-        # The following line will output your MySQL credentials.
-        # print_r(config('database.connections.mysql'));
-
-        echo '<h1>Test Database Connection</h1>';
-        try {
-            $results = DB::select('SHOW DATABASES;');
-            echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-            echo "<br><br>Your Databases:<br><br>";
-            print_r($results);
-        }
-        catch (Exception $e) {
-            echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-        }
-
-        echo '</pre>';
-    });
-
-    /*
-        Route::resource('projects', 'ProjectController');
-        Route::resource('projects.inspections', 'ProjectInspectionController');
-
-        Route::get('/projects/{id?}/inspections', 'ProjectInspectionController@getIndex');
-
-        Route::get('/projects/{id?}/inspections/create', 'ProjectInspectionController@getCreate');
-        Route::post('/projects/{id?}/inspections/create', 'ProjectInspectionController@postCreate');
-
-        Route::get('/projects/{id?}/inspections/{inspectionID}/edit', 'ProjectInspectionController@getEdit');
-        Route::post('/projects/{id?}/inspections/{inspectionID}/edit', 'ProjectInspectionController@postEdit');
-
-        Route::get('/projects/{id?}/inspections/{inspectionID}/show', 'ProjectInspectionController@getShow');
-        Route::post('/projects/{id?}/inspections/{inspectionID}/show', 'ProjectInspectionController@postShow');
-     */
-
-    /*
-    if(App::environment('local')) {
-
-        Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-
-        Route::get('/drop', function() {
-            DB::statement('DROP database foobooks');
-            DB::statement('CREATE database foobooks');
-            return 'Dropped foobooks; created foobooks.';
-        });
-    }
-    */
+    Route::get('/project/about', 'ProjectController@getAbout');
 
     Route::group(['middleware' => 'auth'], function() {
         Route::get('/', 'ProjectController@getIndex');
