e53365d616e402524ac0791a2b784a18bebb74ab
wyu <kharma@gmail.com>
2016-05-12 12:04:23 -0700

    p4: add about page

---
+++ resources/views/projects/about.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+
+@section('title')
+    StormSafe | About Us
+@stop
+
+@section('head')
+    <link href='/css/inspection.css' rel='stylesheet'>
+@stop
+
+@section('content')
+
+    <h1>About StormSafe, Environmental Software</h1>
+    <br>
+    <h3>
+        <p>
+            StormSafe helps Civil & Environmental engineers with environmental compliance.
+        </p>
+        <br>
+
+        <p>
+            Benefits of using our software with your projects are as follows:
+        </p>
+
+        <li>
+            All users to track projects and site inspections to meet environmnetal permit requirements
+        </li>
+        <li>
+            Save users time in filling out, submitting and storing paper forms
+        </li>
+        <li>
+            Improve project record retention by storing them electronically
+        </li>
+        <li>
+            Complete site inspection reports using the website, based on the US EPA site inspection template
+        </li>
+    </h3>
+
+@stop
