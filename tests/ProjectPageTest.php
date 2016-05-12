be79b13e44bc1640d23894a886c12ddd5672e36c
wyu <kharma@gmail.com>
2016-05-11 18:22:27 -0700

    p4: update CRUD unit tests

--- tests/ProjectPageTest.php
+++ tests/ProjectPageTest.php
@@ -22,15 +22,34 @@ class ProjectPageTest extends TestCase
                 ->see('Search Projects')
                 ->dontSee('Welcome to StormSafe!')
 
-                # Test create project page
-                # ->click('Create Project')
-                # ->dontSee('All Projects')
-
-                # Test project/index page
+                # Test index page
                 ->visit('/projects')
                 ->See('All Projects')
                 ->see('Create project')
                 ->see('Search projects')
+                ->dontSee('Welcome to StormSafe!')
+                ;
+        }
+        catch(Exception $e)
+        {
+            echo 'Message: '.$e->getMessage();
+        }
+    }
+
+    public function testProjectCreate()
+    {
+        try
+        {
+            $this->visit('/login')
+                # Test auth/login page
+                ->type('walter@stormsavvy.com', 'email')
+                ->type('helloworld', 'password')
+                ->press('Login')
+
+                # Test dashboard
+                ->see('All Projects')
+                ->see('Create Project')
+                ->see('Search Projects')
                 ->dontSee('Welcome to StormSafe!')
 
                 # Test create project
